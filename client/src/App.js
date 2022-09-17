52a23d2d9d4fcaeaef5f8f313d9071889fa20f46
İlkay Burak Uzunali <31900314+ilkayburakuzunali@users.noreply.github.com>
2022-09-17 21:12:48 +0300

    react-router-dom geliştirmesi

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
-
 import './App.css';
 
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import HomeScreen from './Screens/HomeScreen'
+import CreateScreen from './Screens/CreateScreen'
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Merhaba Dünya</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<HomeScreen/>}/>
+                <Route path="/create" element={<CreateScreen/>}/>
+            </Routes>
+        </Router>
+
+    );
 }
 
 export default App;
