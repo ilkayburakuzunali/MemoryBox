54a3080c80861310f4827316c024419ee107e035
İlkay Burak Uzunali <31900314+ilkayburakuzunali@users.noreply.github.com>
2022-09-25 22:16:45 +0300

    header user kontrol

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -6,10 +6,21 @@ import Navbar from 'react-bootstrap/Navbar';
 
 import {LinkContainer} from 'react-router-bootstrap';
 
-import {FiEdit} from 'react-icons/fi';
+import {useLocation} from "react-router-dom";
+
+import {FiEdit, FiLogOut} from 'react-icons/fi';
 import {MdLogin} from 'react-icons/md';
 
 const Header = () => {
+
+    const location = useLocation()
+    const [user, setUser] = useState()
+
+    useEffect(() => {
+        if (localStorage.getItem('user') && !user) {
+            setUser(JSON.parse(localStorage.getItem('user')))
+        }
+    }, [location, user])
 
     return (
         <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
@@ -20,23 +31,39 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="navbarScroll"/>
                 <Navbar.Collapse id="navbarScroll" className="justify-content-end">
                     <Nav>
-                        <LinkContainer to='/create'>
-                            <Nav.Link>
-                                <Button variant="outline-light">
-                                    <FiEdit className='me-2 mb-1' size={17}/>
-                                    Bir anı paylaş
-                                </Button>
-                            </Nav.Link>
-                        </LinkContainer>
+                        {
+                            user ? (
+                                <>
+                                    <LinkContainer to='/create'>
+                                        <Nav.Link>
+                                            <Button variant="outline-light">
+                                                <FiEdit className='me-2 mb-1' size={17}/>
+                                                Bir anı paylaş
+                                            </Button>
+                                        </Nav.Link>
+                                    </LinkContainer>
 
-                        <LinkContainer to='/auth'>
-                            <Nav.Link>
-                                <Button variant="outline-light">
-                                    <MdLogin className='me-2 mb-1' size={20}/>
-                                    Giriş Yap
-                                </Button>
-                            </Nav.Link>
-                        </LinkContainer>
+
+                                        <Nav.Link>
+                                            <Button variant="outline-light">
+                                                <FiLogOut className='me-2 mb-1' size={17}/>
+                                                Çıkış Yap
+                                            </Button>
+                                        </Nav.Link>
+
+
+                                </>
+                            ) : (
+                                <LinkContainer to='/auth'>
+                                    <Nav.Link>
+                                        <Button variant="outline-light">
+                                            <MdLogin className='me-2 mb-1' size={20}/>
+                                            Giriş Yap
+                                        </Button>
+                                    </Nav.Link>
+                                </LinkContainer>
+                            )
+                        }
                     </Nav>
                 </Navbar.Collapse>
             </Container>
